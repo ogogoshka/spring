accd41b24161abbd5a74701d5ea3e531909e47d3
ogogoshka <xxxpiracy@gmail.com>
2019-06-03 01:06:09 +0700

    removing unused methods

--- src/main/java/com/example/promo/repository/BonusRepository.java
+++ src/main/java/com/example/promo/repository/BonusRepository.java
@@ -8,9 +8,7 @@ import java.util.List;
 
 @Repository
 public interface BonusRepository extends JpaRepository<Bonus, Long> {
-    List<Bonus> findByBonusNameContaining(String filter);
     List<Bonus> findBonusByPromo(Promo promo);
     List<Bonus> findBonusByPromoAndBonusNameContaining(Promo promo, String filter);
-    List<Bonus> findBonusByPromoAndBonusNameContainingAndInStockGreaterThan(Promo promo, String filter, int inStock);
     List<Bonus> findBonusByPromoAndInStockGreaterThan(Promo promo, int inStock);
 }
